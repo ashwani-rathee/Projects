dd216ec689135b3aeb89a9ce02f55892d5f1c51c
ashrathee <54855463+ashrathee@users.noreply.github.com>
2019-12-09 16:48:45 +0530

    ADD.C
    addition

---
+++ C Programs/ADD.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//author:ashwani rathee
+//date-9-12-2019
+int main(void)
+{
+ int a,b,sum;
+ printf("enter two nos: ");
+ scanf("%d %d",&a,&b);
+ sum=a+b;
+ printf("%d + %d is %d",&a,&b,&sum);
+ return 0;
+}
