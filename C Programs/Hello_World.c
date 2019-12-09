c1a169f11a8e19ebf608a43884b9bee277b72e9b
ashrathee <54855463+ashrathee@users.noreply.github.com>
2019-12-09 17:12:41 +0530

    Create Hello_World.c

---
+++ C Programs/Hello_World.c
@@ -0,0 +1,10 @@
+//author:ashwani rathee
+//date:9-12-2019
+#include<stdio.h>
+int main(void)
+{
+ printf("Hello_World");
+ return 0;
+}
+//for printing without using semicolon
+//  if(printf("hello world")){}
