e71d09e170ec7c9fe384b552729c6b14df0e7e4d
ashrathee <54855463+ashrathee@users.noreply.github.com>
2019-12-09 16:55:02 +0530

    Create SUBTRACT.c

---
+++ C Programs/SUBTRACT.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//author:ashwani rathee
+//date:9-12-2019
+int main(void
+{
+ int A,B,Difference;
+ printf("Want to subtracta two nos??enter them here: ");
+ scanf("%d %d",&A,&B);
+ Difference=A-B;
+ printf("DIfference=%d",Difference);
+ return 0;
+}
+ 
